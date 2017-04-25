85ee3420c3164d6faef4d70166f4151b8be186dd
caceres95 <carlos.bcaceres@hotmail.com>
2017-04-24 19:46:33 -0500

    Se elimino boton de reset

--- add_promotion.php
+++ add_promotion.php
@@ -241,7 +241,6 @@
                   <div class="form-group">
                     <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
                       <button class="btn btn-primary" type="button" id="cancel_promotions_button">Cancel</button>
-                      <button class="btn btn-primary" type="reset">Reset</button>
                       <button type="submit" class="btn btn-success" id="add_button_promotions">Submit</button>
                     </div>
                   </div>
